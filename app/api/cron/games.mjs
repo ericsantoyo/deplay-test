8dc60ad1d8cca5217f116341bb690b111bd631ad
Eric Santoyo <eric.yamir@gmail.com>
2023-12-03 20:18:34 -0500

    updated gamesupdate

--- app/api/cron/games.mjs
+++ app/api/cron/games.mjs
@@ -36,9 +36,12 @@ function formatMatches(matches, week) {
   return formattedMatches;
 }
 
-async function fetchData(playerId) {
-  const baseUrl = "https://api-fantasy.llt-services.com";
-  const endpoint = `/api/v3/player/${playerId}?x-lang=en`;
+
+
+async function fetchMatchData(weekID) {
+  const baseUrl = "https://api-fantasy.llt-services.com/stats";
+  const endpoint = `/v1/stats/week/${weekID}?x-lang=en`;
+
   try {
     const response = await fetch(`${baseUrl}${endpoint}`);
     if (response.status === 404 || !response.ok) {
@@ -51,105 +54,26 @@ async function fetchData(playerId) {
   }
 }
 
-async function fetchData(weekID) {
-  const baseUrl = "https://api-fantasy.llt-services.com/stats";
-  const endpoint = `/v1/stats/week/${weekID}?x-lang=en`;
-  try {
-    const response = await fetch(`${baseUrl}${endpoint}`);
-    //up to here updated
-    if (response.status === 404) {
-      return null;
-    }
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-    return null;
-  }
-}
-
-async function fetchMarketValues(playerId) {
-  const baseUrl = "https://api-fantasy.llt-services.com";
-  const endpoint = `/api/v3/player/${playerId}/market-value?x-lang=en`;
-  try {
-    const response = await fetch(`${baseUrl}${endpoint}`);
-    if (response.status === 404 || !response.ok) {
-      return null;
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Fetch market values error:", error);
-    return null;
-  }
-}
-
 async function main() {
   const startingIndex = 0;
-  const endingIndex = 1850;
-  let players = [];
-  const promises = [];
+  const endingIndex = 40;
+  let matches = [];
 
-  for (let playerId = startingIndex; playerId <= endingIndex; playerId++) {
-    promises.push(fetchData(playerId));
-
-    if (promises.length >= 70) {
-      // MAX_CONCURRENT_REQUESTS
-      const playerDataArray = await Promise.all(promises);
-      promises.length = 0; // Reset the promises array
-
-      const marketValuePromises = playerDataArray.map((data) => {
-        if (data) {
-          return fetchMarketValues(data.id);
-        }
-        return null;
-      });
-
-      const marketValuesArray = await Promise.all(marketValuePromises);
-
-      for (let i = 0; i < playerDataArray.length; i++) {
-        const data = playerDataArray[i];
-        if (data) {
-          const marketValues = marketValuesArray[i];
-          data.marketValues = marketValues;
-          players.push(data);
-        }
-      }
-    }
-  }
-
-  // Process any remaining promises
-  if (promises.length > 0) {
-    const playerDataArray = await Promise.all(promises);
-    const marketValuePromises = playerDataArray.map((data) =>
-      data ? fetchMarketValues(data.id) : null
-    );
-    const marketValuesArray = await Promise.all(marketValuePromises);
-
-    for (let i = 0; i < playerDataArray.length; i++) {
-      const data = playerDataArray[i];
-      if (data) {
-        const marketValues = marketValuesArray[i];
-        data.marketValues = marketValues;
-        players.push(data);
-      }
+  for (let weekID = startingIndex; weekID <= endingIndex; weekID++) {
+    const matchData = await fetchMatchData(weekID);
+    if (matchData) {
+      const formattedMatches = formatMatches(matchData);
+      matches.push(...formattedMatches);
     }
   }
 
   try {
-    const { players: playersData, stats: statsData } =
-      splitPlayersData(players);
-    await addPlayers(playersData);
-    await addStats(statsData);
-    console.log("Update successful");
-    process.exit(0); // Exit with a success status code
+    await addMatches(matches);
+    console.log('Schedules update successful');
+    process.exit(0);
   } catch (error) {
-    console.error("Error updating Supabase:", error);
-    process.exit(1); // Exit with an error status code
+    console.error('Error updating schedules in Supabase:', error);
+    process.exit(1);
   }
 }
 
